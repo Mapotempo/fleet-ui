fcb2c7329f301b28a37b549e9fdb3006592ecc92
Jean-Maxime <jm.fillau@gmail.com>
2020-02-04 23:43:02 +0100

    Add badge
    Signed-off-by: Jean-Maxime <jm.fillau@gmail.com>

--- src/components/route/RouteList.js
+++ src/components/route/RouteList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Table, ProgressBar, Label } from 'react-bootstrap';
+import { Table, ProgressBar, Label, Badge } from 'react-bootstrap';
 
 // ==========
 // ROUTE LIST
@@ -16,14 +16,15 @@ const defaultProps = {
 
 const RoutesList = (props) => {
   return (
-    <Table hover responsive>
+    <Table hover responsive striped >
       <thead>
-        <tr>
+        <tr className='info'>
           <th>Name</th>
           <th>Email</th>
+          <th>Phone</th>
           <th>Undone</th>
           <th>Done</th>
-          <th>Missions Count</th>
+          <th>Missions</th>
           <th>Estimated Time Advancement</th>
           <th>Estimated Time Arrival (ETA)</th>
         </tr>
@@ -57,9 +58,10 @@ const RouteItem = (props) => {
     <tr>
       <td>{props.route.name}</td>
       <td>{props.route.user.email}</td>
-      <td>{fake}</td>
-      <td>{props.route.missions.length - fake}</td>
-      <td>{props.route.missions.length}</td>
+      <td>{props.route.user.phone}</td>
+      <td><Badge style={{backgroundColor: "red"}}>{fake}</Badge></td>
+      <td><Badge style={{backgroundColor: "green"}}>{props.route.missions.length - fake}</Badge></td>
+      <td><Badge>{props.route.missions.length}</Badge></td>
       <td ><ProgressBar style={{ margin: 0 }} now={props.route.info.advancing} label={`${props.route.info.advancing}%`} title={`${props.route.info.advancing}%`}/></td>
       <td ><Label bsStyle={style[Math.floor(Math.random() * style.length)]}>{new Date(props.route.info.eta).toLocaleString()}</Label></td>
     </tr>);
